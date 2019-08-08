230ed4286995b1186060ab14eceaa961e8afc801
Niels Justesen <njustesen@gmail.com>
2019-08-08 11:01:33 +0200

    hover click

--- qdboard/static/js/controllers.js
+++ qdboard/static/js/controllers.js
@@ -63,6 +63,7 @@ appControllers.controller('RunShowCtrl', ['$scope', '$routeParams', '$window', '
         $scope.getRun = function getRun(id){
             RunService.get(id).success(function(data) {
                 $scope.run = data;
+                $scope.viridis = d3.scaleSequential().domain([$scope.run.problem.min_fit, $scope.run.problem.max_fit]).interpolator(d3.interpolateViridis);
                 $scope.loadArchive($scope.run_id);
             }).error(function(status, data) {
                 console.log(status);
@@ -80,12 +81,15 @@ appControllers.controller('RunShowCtrl', ['$scope', '$routeParams', '$window', '
             });
         };
 
+        // Color scale
+        $scope.radius = 2;
+
         $scope.drawMap = function drawMap(run, archive) {
 
             // set the dimensions and margins of the graph
             var margin = {top: 10, right: 30, bottom: 30, left: 60},
-                width = 460 - margin.left - margin.right,
-                height = 400 - margin.top - margin.bottom;
+                width = 560 - margin.left - margin.right,
+                height = 500 - margin.top - margin.bottom;
 
             // append the svg object to the body of the page
             var svg = d3.select("#map")
@@ -128,8 +132,34 @@ appControllers.controller('RunShowCtrl', ['$scope', '$routeParams', '$window', '
             svg.append("g")
                 .call(d3.axisLeft(y));
 
-            // Color scale
-            var viridis = d3.scaleSequential().domain([run.problem.min_fit, run.problem.max_fit]).interpolator(d3.interpolateViridis);
+            // gridlines in x axis function
+            function make_x_gridlines() {
+                return d3.axisBottom(x)
+                    .ticks(5)
+            }
+
+            // gridlines in y axis function
+            function make_y_gridlines() {
+                return d3.axisLeft(y)
+                    .ticks(5)
+            }
+
+            // add the X gridlines
+            svg.append("g")
+                .attr("class", "grid")
+                .attr("transform", "translate(0," + height + ")")
+                .call(make_x_gridlines()
+                    .tickSize(-height)
+                    .tickFormat("")
+                );
+
+            // add the Y gridlines
+            svg.append("g")
+                .attr("class", "grid")
+                .call(make_y_gridlines()
+                    .tickSize(-width)
+                    .tickFormat("")
+                );
 
             // Add dots
             svg.append('g')
@@ -139,11 +169,55 @@ appControllers.controller('RunShowCtrl', ['$scope', '$routeParams', '$window', '
                 .append("circle")
                     .attr("cx", function (d) { return x(d.behavior[0]); } )
                     .attr("cy", function (d) { return y(d.behavior[1]); } )
-                    .attr("r", 1.5)
-                    .attr("fill", function(d){return viridis(d.fitness)} )
+                    .attr("r", $scope.radius)
+                    .attr("fill", function(d){return $scope.viridis(d.fitness)} )
+                    .on("mouseover", handleMouseOver)
+                    .on("mouseout", handleMouseOut)
+                    .on("click", handleMousClick)
 
         };
 
+        // Create Event Handlers for mouse
+        function handleMousClick(d, i) {  // Add interactivity
+
+            // Use D3 to select element, change color and size
+            d3.select(this).attr({
+                r: $scope.radius * 4
+            });
+
+            $scope.$apply(function () {
+                $scope.solutionClicked = d;
+            });
+        }
+
+        // Create Event Handlers for mouse
+        function handleMouseOver(d, i) {  // Add interactivity
+
+            // Use D3 to select element, change color and size
+            d3.select(this).attr({
+                r: $scope.radius * 4
+            });
+
+            $scope.$apply(function () {
+                $scope.solutionInFocus = d;
+            });
+        }
+
+        function handleMouseOut(d, i) {
+
+            // Use D3 to select element, change color back to normal
+            d3.select(this).attr({
+                r: $scope.radius
+            });
+
+            $scope.$apply(function () {
+                $scope.solutionInFocus = null;
+            });
+            //console.log($scope.solutionInFocus);
+        }
+
+        $scope.solutionInFocus = null;
+        $scope.solutionClicked = null;
         $scope.run_id = $routeParams.id;
         $scope.getRun($scope.run_id);
 
